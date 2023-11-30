512e7ed2deeb2ecc4070c9a3f66117e5c2b8ee01
Mpho Matli <mphojames9@gmail.com>
2023-11-30 09:49:26 +0200

    update

--- src/index.js
+++ src/index.js
@@ -4,26 +4,30 @@ const locationButton = document.querySelector(".location-btn");
 const currentWeatherDiv = document.querySelector(".current-weather");
 const weatherCardsDiv = document.querySelector(".weather-cards");
 const errorDisplay = document.querySelector(".error_text");
+const daysOfWeek = [
+    'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
+  ];
+
+     const now = new Date();
+     const dayOfWeek = daysOfWeek[now.getDay()];
 
 
 const API_KEY = "c93fd1817f3fbe42aeac0a63076603b9";
 
 const createWeatherCard = (cityName, weatherItem, index) => {
-    const daysOfWeek = [
-        'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
-      ];
-    
-         const now = new Date();
-         const dayOfWeek = daysOfWeek[now.getDay()];
-         
     if(index === 0) { 
         return `<div class="details">
                     <h2>${dayOfWeek} <span>|</span> ${weatherItem.dt_txt.split(" ")[0]}</h2>
                     <h2>${cityName}</h2>
-                    <h6 id="main-temp">${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
+                    <h6 id="main-temp">${(weatherItem.main.temp - 273.15).toFixed(0)}°C</h6>
+                    <div class="min_max">
+                    <h6>L : ${(weatherItem.main.temp_min - 273.15).toFixed(0)}°C</h6>
+                    <span>|</span>
+                    <h6>H : ${(weatherItem.main.temp_max - 273.15).toFixed(0)}°C</h6>
+                    </div>
                     <h6>Feels Like: ${(weatherItem.main.feels_like - 273.15).toFixed(2)}°C</h6>
                     <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
-                    <h6>Humidity: ${weatherItem.main.humidity}%</h6>
+                    <h6>Visibility: ${weatherItem.visibility / 100} KM</h6>
                 </div>
                 <div class="icon">
                     <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
@@ -33,6 +37,7 @@ const createWeatherCard = (cityName, weatherItem, index) => {
         return `<li class="card">
                     <h3>${weatherItem.dt_txt.split(" ")[0]}</h3>
                     <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
+                    <h6>${weatherItem.weather[0].description.toUpperCase()}</h6>
                     <h6>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
                     <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
                     <h6>Humidity: ${weatherItem.main.humidity}%</h6>
@@ -43,6 +48,7 @@ const getWeatherDetails = (cityName, latitude, longitude) => {
     const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
 
     fetch(WEATHER_API_URL).then(response => response.json()).then(data => {
+        console.log(data)
         const uniqueForecastDays = [];
         const fiveDaysForecast = data.list.filter(forecast => {
             const forecastDate = new Date(forecast.dt_txt).getDate();
@@ -68,18 +74,23 @@ const getWeatherDetails = (cityName, latitude, longitude) => {
     });
 }
 
+
 const getCityCoordinates = () => {
     const cityName = cityInput.value.trim();
     if (cityName === "") return;
-    const API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
+
+    else{   
+        const API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
     
-    fetch(API_URL).then(response => response.json()).then(data => {
-        if (!data.length) return noCoordinates();
-        const { lat, lon, name } = data[0];
-        getWeatherDetails(name, lat, lon);
-    }).catch(() => {
-        getCityCoordinatesError();
-    });
+        fetch(API_URL).then(response => response.json()).then(data => {
+            if (!data.length) return noCoordinates();
+            const { lat, lon, name } = data[0];
+            getWeatherDetails(name, lat, lon);
+        }).catch(() => {
+            getCityCoordinatesError();
+        });
+    }
+
 }
 
 const getUserCoordinates = () => {
